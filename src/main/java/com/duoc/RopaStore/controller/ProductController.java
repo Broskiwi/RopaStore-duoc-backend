1828f247d9351778cf159bc38365e0f51ca75356
Broskiwi <ignacio.andres.ibanez@gmail.com>
2026-09-10 01:38:57 -0300

    Create ProductController.java

---
+++ src/main/java/com/duoc/RopaStore/controller/ProductController.java
@@ -0,0 +1,24 @@
+package com.duoc.RopaStore.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<ProductDTO> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+}
